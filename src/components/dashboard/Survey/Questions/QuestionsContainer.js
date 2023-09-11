a04157caf1da47524ea5989fe53d4ec70b20f11a
dravydev <dravy67@gmail.com>
2023-09-12 01:22:32 +0200

    survey
    survey dnd, better options menu

--- src/components/dashboard/Survey/Questions/QuestionsContainer.js
+++ src/components/dashboard/Survey/Questions/QuestionsContainer.js
@@ -2,30 +2,76 @@ import styles from './questions.module.scss'
 
 import QuestionsContainerItem from './QuestionsContainerItem'
 
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
+import { useCallback, useMemo, useState } from 'react'
+
 const QuestionsContainer = () => {
+
+    const tempQuestions = useMemo(() => {
+        return [
+            {
+                id: 'hw-1',
+                title: 'Pytanie 1',
+                description: 'Opis',
+                isDescription: true,
+                isRequired: true,
+                mode: 'shortText'
+            },
+            {
+                id: 'hw-2',
+                title: 'Pytanie 2',
+                description: 'Opis',
+                isDescription: true,
+                isRequired: true,
+                mode: 'shortText'
+            },
+            {
+                id: 'hw-3',
+                title: 'Pytanie 3',
+                description: 'Opis',
+                isDescription: true,
+                isRequired: true,
+                mode: 'shortText'
+            }
+        ]
+    }, [])
+
+    const [questions, setQuestions] = useState(tempQuestions)
+
+    const onDragEnd = useCallback(event => {
+
+        const { source, destination } = event
+
+        if (!destination) return
+
+        const [itemToMove] = questions.splice(source.index, 1)
+        questions.splice(destination.index, 0, itemToMove)
+
+        setQuestions([ ...questions ])
+
+    }, [])
+
     return (
         <div className={styles.container}>
-            <QuestionsContainerItem
-                title="Jakiego koloru jest twoje bugatti?"
-            />
-            <QuestionsContainerItem
-                title="Jakiego koloru jest twoje bugatti?"
-            />
-            <QuestionsContainerItem
-                title="Jakiego koloru jest twoje bugatti?"
-            />
-            <QuestionsContainerItem
-                title="Jakiego koloru jest twoje bugatti?"
-            />
-            <QuestionsContainerItem
-                title="Jakiego koloru jest twoje bugatti?"
-            />
-            <QuestionsContainerItem
-                title="Jakiego koloru jest twoje bugatti?"
-            />
-            <QuestionsContainerItem
-                title="Jakiego koloru jest twoje bugatti?"
-            />
+            <DragDropContext onDragEnd={onDragEnd}>
+                <Droppable
+                    getContainerForClone={true}
+                    droppableId="questions"
+                    direction="vertical"
+                >
+
+                    {provided => (
+                        <div ref={provided.innerRef}>
+                            {questions.map((question, index) => <QuestionsContainerItem
+                                key={question.id}
+                                {...question}
+                                index={index}
+                            />)}
+                        </div>
+                    )}
+
+                </Droppable>
+            </DragDropContext>
         </div>
     )
 }
--- src/components/dashboard/Survey/Questions/QuestionsContainerItem.js
+++ src/components/dashboard/Survey/Questions/QuestionsContainerItem.js
@@ -2,21 +2,50 @@ import styles from './questions.module.scss'
 
 import Input from '@/components/ui/Input'
 
+import { PiDotsSix } from 'react-icons/pi'
+
 import cn from '@/utils/cn'
-import { PiDotsSix} from 'react-icons/pi'
+
+import { Draggable } from 'react-beautiful-dnd'
 
 const QuestionsContainerItem = ({ ...props }) => {
     return (
-        <div className={cn(styles.containerItem, styles.block)}>
-            <div className={styles.containerItemMove}>
-                <PiDotsSix />
-            </div>
-            <Input
-                name="title"
-                label="Tytuł pytania"
-                defaultValue={props.title}
-            />
-        </div>
+        <Draggable draggableId={props.id} index={props.index}>
+
+            {(provided, snapshot) => {
+
+                const isActive = snapshot.isDragging ? styles.containerItemActive : ''
+
+                const transform = provided.draggableProps.style.transform
+
+                const style = {
+                    ...provided.draggableProps.style,
+                    transform: transform?.replace(/\(([^,]+),/, '(0px,')
+                }
+
+                return (
+                    <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        style={style}
+                        className={cn(styles.containerItem, isActive, styles.block)}
+                    >
+                        <div
+                            {...provided.dragHandleProps}
+                            className={styles.containerItemMove}
+                        >
+                            <PiDotsSix />
+                        </div>
+                        <Input
+                            name="title"
+                            label="Tytuł pytania"
+                            defaultValue={props.title}
+                        />
+                    </div>
+                )
+            }}
+
+        </Draggable>
     )
 }
 
