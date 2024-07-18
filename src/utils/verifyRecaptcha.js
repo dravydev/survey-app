d666e08eff096355ce4977359df890114ec9f211
dravydev <dravy67@gmail.com>
2024-07-18 13:03:45 +0200

    y

--- src/utils/verifyRecaptcha.js
+++ src/utils/verifyRecaptcha.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const verifyRecaptcha = async (token) => {
+
 	const request = await axios(
 		`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET}&response=${token}`
 	)
